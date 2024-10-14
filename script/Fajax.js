cf1cc9531fb103479d956a3935ed6be52fe5714f
Naama18 <naamabiton@hilma.tech>
2024-10-14 10:13:10 +0300

    second Fajax draft+data changes

--- script/Fajax.js
+++ script/Fajax.js
@@ -1,13 +1,12 @@
 class Fajax{
     constructor(){
-        this.openFajax(typeOfRequest,url,content)
+        this.openFajax(typeOfRequst,url,content)
         {
-            //returns ["GET",url]/["POST",url,content] based on what the client does
+            //returns ["GET",url]/["POST",url] based on what the client does
         },
         this.sendFajax()
         {},
-        this.onloadFajax()
-        {} 
+        this.onloadFajax(){} 
         // when it gets the action from the client it starts the connection
 
 
--- script/data.js
+++ script/data.js
@@ -6,29 +6,20 @@ const stringifyContent = JSON.stringify(allContent);
 localStorage.setItem("Naama", stringifyContent);
 localStorage.setItem("Current user", "");
 
-function insertRegisterValidation() {
-  //   let allData = JSON.parse(localStorage.getItem("allContent"));
-  //   alert(allData);
-
-  let name = document.getElementById("pName").value;
-  let password = document.getElementById("password").value;
+function insertRegisterValidation(name, password) {
   let nameIsExist = localStorage.getItem(name);
-  console.log("nameIsExist: ", nameIsExist);
   if (nameIsExist === null) {
     const userData = {
       password: password,
       content: {},
     };
-    // allData[name] = JSON.stringify(userData);
     localStorage.setItem(name, JSON.stringify(userData));
   } else {
     alert("userName already exist");
   }
 }
 
-function logInValidation() {
-  let name = document.getElementById("pName").value;
-  let password = document.getElementById("password").value;
+function logInValidation(name, password) {
   const storedUserData = localStorage.getItem(name);
   if (storedUserData) {
     const userData = JSON.parse(storedUserData);
@@ -50,9 +41,7 @@ function valInarray(val, arr) {
   return false;
 }
 
-function AddToContacts() {
-  let nameToAdd = document.getElementById("name").value;
-  let phoneNumber = document.getElementById("phonetoadd").value;
+function AddToContacts(nameToAdd, phoneNumber) {
   let currentUser = localStorage.getItem("Current user");
   let getFromLocal = JSON.parse(localStorage.getItem(currentUser));
   console.log(getFromLocal.content);
