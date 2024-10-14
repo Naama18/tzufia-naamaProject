bf3a567a02ebd646b8a2a82755b3e7549e4d2abc
Naama18 <naamabiton@hilma.tech>
2024-10-14 08:46:30 +0300

    database commends

--- script/index.js
+++ script/index.js
@@ -1,30 +1,29 @@
 function showContentlog() {
-    document.getElementById("shown").innerHTML=" ";
-    let temp, item, a, i;
-    temp = document.getElementsByTagName("template")[0];
-    item = temp.content.querySelector("div");
-    a = document.importNode(item, true);
-    document.getElementById("shown").appendChild(a);
-  }
+  document.getElementById("shown").innerHTML = " ";
+  let temp, item, a, i;
+  temp = document.getElementsByTagName("template")[0];
+  item = temp.content.querySelector("div");
+  a = document.importNode(item, true);
+  document.getElementById("shown").appendChild(a);
+}
 
+function showContentreg() {
+  document.getElementById("shown").innerHTML = " ";
+  let temp, item, a, i;
+  temp = document.getElementsByTagName("template")[1];
+  item = temp.content.querySelector("div");
+  a = document.importNode(item, true);
+  document.getElementById("shown").appendChild(a);
+}
 
-  function showContentreg() {
-    document.getElementById("shown").innerHTML=" ";
-    let temp, item, a, i;
-    temp = document.getElementsByTagName("template")[1];
-    item = temp.content.querySelector("div");
-    a = document.importNode(item, true);
-    document.getElementById("shown").appendChild(a);
-  }
-
-  function showContentContacts() {
-    document.getElementById("shown").innerHTML=" ";
-    let temp, item, a, i;
-    temp = document.getElementsByTagName("template")[2];
-    item = temp.content.querySelector("div");
-    a = document.importNode(item, true);
-    document.getElementById("shown").appendChild(a);
-  }
+function showContentContacts() {
+  document.getElementById("shown").innerHTML = " ";
+  let temp, item, a, i;
+  temp = document.getElementsByTagName("template")[2];
+  item = temp.content.querySelector("div");
+  a = document.importNode(item, true);
+  document.getElementById("shown").appendChild(a);
+}
 
 //   function showContentAddContacts() {
 //     document.getElementById("shown").innerHTML=" ";
